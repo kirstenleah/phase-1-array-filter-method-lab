a7bf8cfdac734b98873b1735906997bc07480b71
Kirsten <kirsten.staggs@gmail.com>
2022-04-09 12:01:08 -0600

    commit tree 0a9abff38effb88e57cdb984b8a1f3e164e99aec

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+// function findMatching(driversArr, driverName) {
+// const newDriverArr = driversArr.filter(element => driverName.toLowerCase() === element.toLowerCase()) 
+// console.log('this is the array', newDriverArr);
+// return newDriverArr;
+// }
+
+// function fuzzyMatch(drivers, startsWith) {
+//     return drivers.filter(
+//         (saMatch) => saMatch.toLowerCase().indexOf(startsWith.toLowerCase()) === 0      
+//     );
+// };
+
+// function matchName(drivers, elementMatch) {
+//     return drivers.filter((fileReturn) => fileReturn.name === elementMatch);
+// };
+
+function findMatching(drivers, drivName) {
+    return drivers.filter(element => element.toLowerCase() === drivName.toLowerCase())
+}
+
+function fuzzyMatch(drivers, lettMatch) {
+    return drivers.filter(element => element.indexOf(lettMatch) === 0, 1)
+}
+
+function matchName(drivers, hometownObjStr) {
+    return drivers.filter(element => element.name === hometownObjStr)
+}
